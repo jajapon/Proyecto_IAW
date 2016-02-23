2d6b10dc3375fe4a76862a5a3bc778c378c4bbbf
Juan Antonio Japon de la Torre <juan.antonio.japon@gmail.com>
2016-02-23 10:58:03 +0100

    Subida prueba openshifht 12

--- PhoneJapan/php/insertar_opinion.php
+++ PhoneJapan/php/insertar_opinion.php
@@ -1,22 +1,18 @@
 <?php
     session_start();
-    $connection = new mysqli("localhost", "root", "", "phonejapan");
-                                    
-    $consulta = "SELECT * FROM USUARIO WHERE USERNAME ='".$_SESSION["usuario"]."';"; 
+    include("./conexion.php");                                
+    $consulta = "SELECT * FROM USUARIO WHERE USERNAME ='".$_SESSION["usuario"]."';";
     $user="";
     $prod=$_POST["codprod"];
     $mens=$_POST["addComment"];
-    if ($connection->connect_errno) {
-            printf("Connection failed: %s\n", $connection->connect_error);
-            exit();
-     }
+
     if ($result = $connection->query($consulta)) {
         if ($result->num_rows==0) {
-            
+
         }else{
             while($obj=$result->fetch_object()){
                   $user = $obj->COD_USU;
-            }   
+            }
             $consulta = "INSERT INTO OPINION VALUES(NULL,$user,$prod,CURRENT_TIMESTAMP(),'$mens')";
             if ($connection->query($consulta)) {
 
