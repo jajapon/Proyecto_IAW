1c01a0a98121b3e7a84ab269c942c92ad8add142
Juan Antonio Japon de la Torre <juan.antonio.japon@gmail.com>
2016-02-24 09:05:39 +0100

    arreglos openshift 2

--- PhoneJapan/php/borrar_opinion.php
+++ PhoneJapan/php/borrar_opinion.php
@@ -2,5 +2,5 @@
     include("./conexion.php");
     $codOpi = $_POST["codopi"];
     $consulta = "DELETE FROM OPINION WHERE COD_OPINION = ".$codOpi;
-    $mysqli->query("DELETE FROM OPINION WHERE COD_OPINION = ".$codOpi);
+    $connection->query("DELETE FROM OPINION WHERE COD_OPINION = ".$codOpi);
 ?>
--- PhoneJapan/ver_perfiluser.php
+++ PhoneJapan/ver_perfiluser.php
@@ -99,12 +99,8 @@
                       <?php
                       $user = $_SESSION["usuario"];
                       $consulta = "SELECT * FROM USUARIO WHERE USERNAME = '$user';";
-                      $connection = new mysqli("localhost", "root", "", "phonejapan");
                                                     //TESTING IF THE CONNECTION WAS RIGHT
-                      if ($connection->connect_errno) {
-                               printf("Connection failed: %s\n", $connection->connect_error);
-                               exit();
-                      }
+                      include("./php/conexion.php");
                       if ($result = $connection->query($consulta)) {
                           if ($result->num_rows==0) {
 
