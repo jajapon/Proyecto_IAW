603e0606aeca9b022e765e8fc7f3f3644ff2ceb8
Juan Antonio Japon de la Torre <juan.antonio.japon@gmail.com>
2016-02-26 12:13:04 +0100

    cambios finales 3

--- phonejapan.sql
+++ phonejapan.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
+-- version 4.0.10.12
 -- http://www.phpmyadmin.net
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 20-01-2016 a las 08:05:31
--- Versión del servidor: 5.6.26
--- Versión de PHP: 5.6.12
+-- Servidor: 127.2.0.2:3306
+-- Tiempo de generación: 26-02-2016 a las 11:09:43
+-- Versión del servidor: 5.5.45
+-- Versión de PHP: 5.3.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de datos: `phonejapan`
@@ -23,11 +23,11 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `caracteristicas`
+-- Estructura de tabla para la tabla `CARACTERISTICAS`
 --
 
 CREATE TABLE IF NOT EXISTS `CARACTERISTICAS` (
-  `COD_CARAC` int(6) NOT NULL,
+  `COD_CARAC` int(6) NOT NULL AUTO_INCREMENT,
   `COD_PROD` int(6) DEFAULT NULL,
   `PANTALLA` varchar(60) DEFAULT NULL,
   `RESOLUCION` varchar(60) DEFAULT NULL,
@@ -37,11 +37,13 @@ CREATE TABLE IF NOT EXISTS `CARACTERISTICAS` (
   `SO` varchar(60) DEFAULT NULL,
   `FRONTAL` varchar(60) DEFAULT NULL,
   `TRASERA` varchar(60) DEFAULT NULL,
-  `SIM` varchar(60) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
+  `SIM` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`COD_CARAC`),
+  KEY `FK_CAR_PROD` (`COD_PROD`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=34 ;
 
 --
--- Volcado de datos para la tabla `caracteristicas`
+-- Volcado de datos para la tabla `CARACTERISTICAS`
 --
 
 INSERT INTO `CARACTERISTICAS` (`COD_CARAC`, `COD_PROD`, `PANTALLA`, `RESOLUCION`, `RAM`, `MINTERNA`, `PROCESADOR`, `SO`, `FRONTAL`, `TRASERA`, `SIM`) VALUES
@@ -64,30 +66,35 @@ INSERT INTO `CARACTERISTICAS` (`COD_CARAC`, `COD_PROD`, `PANTALLA`, `RESOLUCION`
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `cesta`
+-- Estructura de tabla para la tabla `CESTA`
 --
 
 CREATE TABLE IF NOT EXISTS `CESTA` (
   `COD_USU` int(6) NOT NULL DEFAULT '0',
   `COD_PROD` int(8) NOT NULL DEFAULT '0',
-  `CANTIDAD` int(10) DEFAULT NULL
+  `CANTIDAD` int(10) DEFAULT NULL,
+  PRIMARY KEY (`COD_USU`,`COD_PROD`),
+  KEY `FK_CEST_PROD` (`COD_PROD`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `lineadepedido`
+-- Estructura de tabla para la tabla `LINEADEPEDIDO`
 --
 
 CREATE TABLE IF NOT EXISTS `LINEADEPEDIDO` (
-  `COD_LINEA` int(8) NOT NULL,
+  `COD_LINEA` int(8) NOT NULL AUTO_INCREMENT,
   `COD_PEDIDO` int(6) NOT NULL DEFAULT '0',
   `COD_PROD` int(6) NOT NULL DEFAULT '0',
-  `CANTIDAD` int(6) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=latin1;
+  `CANTIDAD` int(6) DEFAULT NULL,
+  PRIMARY KEY (`COD_LINEA`,`COD_PEDIDO`,`COD_PROD`),
+  KEY `FK_PED_LP` (`COD_PEDIDO`),
+  KEY `FK_PED_P` (`COD_PROD`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=40 ;
 
 --
--- Volcado de datos para la tabla `lineadepedido`
+-- Volcado de datos para la tabla `LINEADEPEDIDO`
 --
 
 INSERT INTO `LINEADEPEDIDO` (`COD_LINEA`, `COD_PEDIDO`, `COD_PROD`, `CANTIDAD`) VALUES
@@ -98,21 +105,29 @@ INSERT INTO `LINEADEPEDIDO` (`COD_LINEA`, `COD_PEDIDO`, `COD_PROD`, `CANTIDAD`) 
 (31, 2, 10, 1),
 (32, 2, 14, 2),
 (33, 3, 10, 2),
-(34, 4, 14, 1);
+(34, 4, 14, 1),
+(35, 5, 14, 2),
+(36, 6, 7, 2),
+(37, 7, 7, 2),
+(38, 8, 6, 2),
+(39, 8, 14, 3);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `opinion`
+-- Estructura de tabla para la tabla `OPINION`
 --
 
 CREATE TABLE IF NOT EXISTS `OPINION` (
-  `COD_OPINION` int(20) NOT NULL,
+  `COD_OPINION` int(20) NOT NULL AUTO_INCREMENT,
   `COD_USU` int(6) DEFAULT NULL,
   `COD_PROD` int(8) DEFAULT NULL,
   `FECHA_OP` date DEFAULT NULL,
-  `MENSAJE` varchar(1000) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=latin1;
+  `MENSAJE` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`COD_OPINION`),
+  KEY `FK_OP_USU` (`COD_USU`),
+  KEY `FK_OP_PROD` (`COD_PROD`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=122 ;
 
 --
 -- Volcado de datos para la tabla `OPINION`
@@ -121,30 +136,43 @@ CREATE TABLE IF NOT EXISTS `OPINION` (
 INSERT INTO `OPINION` (`COD_OPINION`, `COD_USU`, `COD_PROD`, `FECHA_OP`, `MENSAJE`) VALUES
 (86, 6, 15, '2016-01-15', 'Este movil funciona de maravilla, 0 problemas hasta el momento usandolo. Gracias por todo PhoneJapan. Buen trato al cliente'),
 (87, 6, 11, '2016-01-15', 'Este producto va de maravilla, lo recomiendo'),
-(88, 6, 10, '2016-01-15', 'Necesitabaaa este producto, va de maravilla'),
 (89, 7, 14, '2016-01-15', 'Producto en optimas condiciones. lo recomiendo'),
 (90, 7, 2, '2016-01-15', 'Funciona de maravilla, no he tenido problemas hasta el momento'),
-(92, 7, 10, '2016-01-15', 'No te motives, este japon es un caso haciendo cosas jajaja'),
-(94, 3, 10, '2016-01-15', 'Como sigais hablando mal de japon os borro la cuenta y ya no volveis a entrar aqui x,DDD JAPON CABROOON SALUDAA A LA AFICION!!'),
-(95, 1, 10, '2016-01-15', 'Que mamoooones sois jajaja\n'),
-(96, 10, 10, '2016-01-15', 'Japooon me tienes que ayudar con lo de tito nicoo que voy mas perdio que el barco del arrooo!'),
-(97, 12, 10, '2016-01-15', 'Japon que te acabo de hacer un SQL Injectioon '),
-(98, 22, 10, '2016-01-15', 'jaapon necesito ayudaa, podrias pasarme todo el proyecto? para verlo y eso (luego lo entrego tal y como esta)'),
-(99, 7, 10, '2016-01-15', 'esto esta to maal, el proximo dia te voy a petar la pagina jajajaj'),
-(100, 23, 10, '2016-01-15', 'Ayyyyy japooon pero k vieeen echaa staaaa. Jose daniiieee k me avurroooo ablamee por waaa');
+(101, 6, 14, '2016-02-23', 'ds'),
+(102, 6, 14, '2016-02-23', 'dsiou'),
+(104, 7, 1, '2016-02-23', 'safsafa'),
+(105, 7, 1, '2016-02-23', 'iklosjifasdklÃ±fjs'),
+(106, 7, 1, '2016-02-23', 'jejeje'),
+(107, 7, 1, '2016-02-23', '<?php\necho "mal"\n?>'),
+(108, 7, 1, '2016-02-23', 'holaaa'),
+(109, 7, 1, '2016-02-23', '<?php\necho "mal"\n?>'),
+(110, 7, 1, '2016-02-23', '<?php\necho $_SESSION["user];\n?>'),
+(111, 7, 1, '2016-02-23', '<?php\necho "<h1>hello</h1>;\n?>'),
+(112, 7, 1, '2016-02-23', '<?php\necho "<h1>hello</h1>";\n?>'),
+(113, 7, 1, '2016-02-23', '<?php\nprint(<h1>hello</h1>);\n?>'),
+(114, 7, 1, '2016-02-23', '<?php\nprint("<h1>hello</h1>");\n?>'),
+(115, 7, 1, '2016-02-23', '<?php\nprintf("<h1>hello</h1>");\n?>'),
+(116, 7, 1, '2016-02-23', '<?php\n5+5\n?>'),
+(117, 7, 1, '2016-02-23', '<?php\necho 10+19\n?>'),
+(118, 7, 1, '2016-02-23', '<?php\necho 10+19;\n?>'),
+(119, 7, 1, '2016-02-23', '<?php\necho 5*6;\n?>'),
+(120, 10, 7, '2016-02-24', 'kjlkj'),
+(121, 26, 10, '2016-02-26', 'Producto en optimas condiciones');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `pedido`
+-- Estructura de tabla para la tabla `PEDIDO`
 --
 
 CREATE TABLE IF NOT EXISTS `PEDIDO` (
-  `COD_PEDIDO` int(6) NOT NULL,
+  `COD_PEDIDO` int(6) NOT NULL AUTO_INCREMENT,
   `COD_USU` int(6) DEFAULT NULL,
   `FECHA_PED` date DEFAULT NULL,
-  `IMPORTE` double(11,2) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+  `IMPORTE` double(11,2) DEFAULT NULL,
+  PRIMARY KEY (`COD_PEDIDO`),
+  KEY `FK_PED_USU` (`COD_USU`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Volcado de datos para la tabla `PEDIDO`
@@ -154,7 +182,11 @@ INSERT INTO `PEDIDO` (`COD_PEDIDO`, `COD_USU`, `FECHA_PED`, `IMPORTE`) VALUES
 (1, 6, '2016-01-15', 767.00),
 (2, 7, '2016-01-15', 2217.00),
 (3, 12, '2016-01-15', 358.00),
-(4, 11, '2016-01-16', 329.00);
+(4, 11, '2016-01-16', 329.00),
+(5, 6, '2016-02-23', 658.00),
+(6, 7, '2016-02-23', 398.00),
+(7, 10, '2016-02-24', 398.00),
+(8, 25, '2016-02-24', 1185.00);
 
 -- --------------------------------------------------------
 
@@ -163,16 +195,17 @@ INSERT INTO `PEDIDO` (`COD_PEDIDO`, `COD_USU`, `FECHA_PED`, `IMPORTE`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `PRODUCTO` (
-  `COD_PROD` int(8) NOT NULL,
+  `COD_PROD` int(8) NOT NULL AUTO_INCREMENT,
   `MARCA` varchar(50) DEFAULT NULL,
   `MODELO` varchar(50) DEFAULT NULL,
   `STOCK` int(8) DEFAULT NULL,
   `IMAGEN` varchar(1000) DEFAULT NULL,
-  `PRECIO_UNI` double(11,2) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+  `PRECIO_UNI` double(11,2) DEFAULT NULL,
+  PRIMARY KEY (`COD_PROD`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
 --
--- Volcado de datos para la tabla `producto`
+-- Volcado de datos para la tabla `PRODUCTO`
 --
 
 INSERT INTO `PRODUCTO` (`COD_PROD`, `MARCA`, `MODELO`, `STOCK`, `IMAGEN`, `PRECIO_UNI`) VALUES
@@ -181,15 +214,15 @@ INSERT INTO `PRODUCTO` (`COD_PROD`, `MARCA`, `MODELO`, `STOCK`, `IMAGEN`, `PRECI
 (3, 'Samsung', 'Galaxy S6 G920 Blanco', 50, './imagenes/productos/samsung_galaxy_s6_690_blanco.png', 690.00),
 (4, 'Samsung', 'Galaxy J5 Blanco', 50, './imagenes/productos/SAMSUNG GALAXY J5 BLANCO.png', 219.00),
 (5, 'Kazam', 'Trooper 450 Negro', 50, './imagenes/productos/KAZAM TROOPER 450 NEGRO.PNG', 99.00),
-(6, 'ZTE', 'Blade L3 Blanco', 50, './imagenes/productos/ZTE BLADE L3 BLANCO.png', 99.00),
-(7, 'Huawei', 'P8 LITE', 50, './imagenes/productos/HUAWEI P8 LITE.png', 199.00),
+(6, 'ZTE', 'Blade L3 Blanco', 48, './imagenes/productos/ZTE BLADE L3 BLANCO.png', 99.00),
+(7, 'Huawei', 'P8 LITE', 46, './imagenes/productos/HUAWEI P8 LITE.png', 199.00),
 (8, 'Huawei', 'Y625 Negro', 50, './imagenes/productos/HUAWEI Y625 NEGRO.PNG', 119.00),
 (9, 'Huawei', 'P8 LITE Blanco', 50, './imagenes/productos/HUAWEI P8 LITE BLANCO.png', 199.00),
 (10, 'BQ', 'Aquaris M4,5 QHD 4G (8+1GB) Blanco', 46, './imagenes/productos/BQ AQUARIS M4,5 QHD 4G (8+1GB) BLANCO.png', 179.00),
 (11, 'LG', 'Leon 4G Titanio', 49, './imagenes/productos/LG LEON 4G TITANIO.PNG', 159.00),
 (12, 'BQ ', 'Aquaris A4.5 QHD 4G (16+1GB) Blanco', 100, './imagenes/productos/BQ AQUARIS A4.5 QHD 4G (16+1GB) BLANCO.png', 179.00),
 (13, 'ZTE ', 'Blade A452 Dorado', 50, './imagenes/productos/ZTE BLADE A452 DORADO.png', 149.00),
-(14, 'Huawei', 'G8 Silver', 47, './imagenes/productos/HUAWEI G8 SILVER.png', 329.00),
+(14, 'Huawei', 'G8 Silver', 42, './imagenes/productos/HUAWEI G8 SILVER.png', 329.00),
 (15, 'Sony', 'Xperia  Z3', 49, './imagenes/productos/29907_spc_220_330_qhigh_product.PNG', 429.00);
 
 -- --------------------------------------------------------
@@ -199,15 +232,16 @@ INSERT INTO `PRODUCTO` (`COD_PROD`, `MARCA`, `MODELO`, `STOCK`, `IMAGEN`, `PRECI
 --
 
 CREATE TABLE IF NOT EXISTS `PROVEEDOR` (
-  `COD_PROV` int(4) NOT NULL,
+  `COD_PROV` int(4) NOT NULL AUTO_INCREMENT,
   `NOMBRE` varchar(40) DEFAULT NULL,
   `CIUDAD` varchar(40) DEFAULT NULL,
   `DIRECCION` varchar(40) DEFAULT NULL,
-  `CP` int(5) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+  `CP` int(5) DEFAULT NULL,
+  PRIMARY KEY (`COD_PROV`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
 --
--- Volcado de datos para la tabla `proveedor`
+-- Volcado de datos para la tabla `PROVEEDOR`
 --
 
 INSERT INTO `PROVEEDOR` (`COD_PROV`, `NOMBRE`, `CIUDAD`, `DIRECCION`, `CP`) VALUES
@@ -222,19 +256,22 @@ INSERT INTO `PROVEEDOR` (`COD_PROV`, `NOMBRE`, `CIUDAD`, `DIRECCION`, `CP`) VALU
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `suministro`
+-- Estructura de tabla para la tabla `SUMINISTRO`
 --
 
 CREATE TABLE IF NOT EXISTS `SUMINISTRO` (
-  `COD_SUM` int(8) NOT NULL,
+  `COD_SUM` int(8) NOT NULL AUTO_INCREMENT,
   `COD_PROD` int(8) DEFAULT NULL,
   `COD_PROV` int(8) DEFAULT NULL,
   `CANTIDAD` int(8) DEFAULT NULL,
-  `FECHA_SUM` date NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
+  `FECHA_SUM` date NOT NULL,
+  PRIMARY KEY (`COD_SUM`),
+  KEY `FK_SUM_PROD` (`COD_PROD`),
+  KEY `FK_SUM_PROV` (`COD_PROV`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
 
 --
--- Volcado de datos para la tabla `suministro`
+-- Volcado de datos para la tabla `SUMINISTRO`
 --
 
 INSERT INTO `SUMINISTRO` (`COD_SUM`, `COD_PROD`, `COD_PROV`, `CANTIDAD`, `FECHA_SUM`) VALUES
@@ -257,11 +294,11 @@ INSERT INTO `SUMINISTRO` (`COD_SUM`, `COD_PROD`, `COD_PROV`, `CANTIDAD`, `FECHA_
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuario`
+-- Estructura de tabla para la tabla `USUARIO`
 --
 
 CREATE TABLE IF NOT EXISTS `USUARIO` (
-  `COD_USU` int(6) NOT NULL,
+  `COD_USU` int(6) NOT NULL AUTO_INCREMENT,
   `USERNAME` varchar(60) DEFAULT NULL,
   `USERPASS` varchar(60) DEFAULT NULL,
   `ROLE` varchar(20) DEFAULT NULL,
@@ -276,11 +313,13 @@ CREATE TABLE IF NOT EXISTS `USUARIO` (
   `CIUDAD` varchar(60) DEFAULT NULL,
   `PROVINCIA` varchar(60) DEFAULT NULL,
   `PAIS` varchar(60) DEFAULT NULL,
-  `TLF` int(9) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
+  `TLF` int(9) DEFAULT NULL,
+  PRIMARY KEY (`COD_USU`),
+  UNIQUE KEY `USERNAME` (`USERNAME`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
 
 --
--- Volcado de datos para la tabla `usuario`
+-- Volcado de datos para la tabla `USUARIO`
 --
 
 INSERT INTO `USUARIO` (`COD_USU`, `USERNAME`, `USERPASS`, `ROLE`, `ESTADO`, `EMAIL`, `NOMBRE`, `APELLIDOS`, `DNI`, `FECHA_NAC`, `DIRECCION`, `CP`, `CIUDAD`, `PROVINCIA`, `PAIS`, `TLF`) VALUES
@@ -290,124 +329,14 @@ INSERT INTO `USUARIO` (`COD_USU`, `USERNAME`, `USERPASS`, `ROLE`, `ESTADO`, `EMA
 (7, 'adrian', '81dc9bdb52d04dc20036dbd8313ed055', 'User', 'ON', 'adriancasado@gmail.com', 'Adrian', 'Casado', '12345678D', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 (8, 'juanfran', '81dc9bdb52d04dc20036dbd8313ed055', 'User', 'ON', 'juanfransanc@gmail.com', 'Juanfran', 'Sanchez', '12345678D', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 (9, 'matito', '81dc9bdb52d04dc20036dbd8313ed055', 'User', 'ON', 'matito@gmail.com', 'Jesus', 'Matito', '12345678D', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(10, 'merino', '827ccb0eea8a706c4c34a16891f84e7b', 'User', 'ON', 'merineitor@gmail.com', 'Antonio', 'Merino', '12345678D', '2015-12-01', 'Merino Street', 41010, 'Sevilla', 'Sevilla', 'EspaÃ±a', 923333333),
+(10, 'merino', '827ccb0eea8a706c4c34a16891f84e7b', 'User', 'OFF', 'merineitor@gmail.com', 'Antonio', 'Merino', '12345678D', '2015-12-01', 'Merino Street', 41010, 'Sevilla', 'Sevilla', 'EspaÃ±a', 923333333),
 (11, 'ajvjimenez', '81dc9bdb52d04dc20036dbd8313ed055', 'User', 'ON', 'ajvjimenez@gmail.com', 'Antonio', 'Jimenez', '12345678D', '1991-01-15', 'C/ Alfareria 1ÂºB', 41010, 'Sevilla', 'Sevilla', 'EspaÃ±a', 999999999),
 (12, 'javi', '81dc9bdb52d04dc20036dbd8313ed055', 'User', 'ON', 'javimawy@gmail.com', 'Javi', 'Mawy', '12345678D', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 (22, 'Noelia', '81dc9bdb52d04dc20036dbd8313ed055', 'User', 'ON', 'noelia@gmail.com', 'Noelia', 'Marin Carrasco', NULL, '2016-01-13', NULL, NULL, NULL, NULL, NULL, NULL),
-(23, 'carolyne', '81dc9bdb52d04dc20036dbd8313ed055', 'User', 'ON', 'carolynenicole@hotmail.com', 'Carolyne', 'Nicole', NULL, '2016-01-12', NULL, NULL, NULL, NULL, NULL, NULL);
+(23, 'carolyne', '81dc9bdb52d04dc20036dbd8313ed055', 'User', 'ON', 'carolynenicole@hotmail.com', 'Carolyne', 'Nicole', NULL, '2016-01-12', NULL, NULL, NULL, NULL, NULL, NULL),
+(25, 'jduser', '81dc9bdb52d04dc20036dbd8313ed055', 'User', 'ON', 'pekechis@gmail.com', 'Juan Diego', 'Perez', NULL, '1988-02-09', NULL, NULL, NULL, NULL, NULL, NULL),
+(26, 'jdadmin', '81dc9bdb52d04dc20036dbd8313ed055', 'Admin', 'ON', 'pekechis_admin@gmail.com', 'Juan Diego', 'Perez', '77777777D', '1988-01-05', 'IES Triana', 41020, 'Sevilla', 'Sevilla', 'EspaÃ±a', 644555333);
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `caracteristicas`
---
-ALTER TABLE `CARACTERISTICAS`
-  ADD PRIMARY KEY (`COD_CARAC`),
-  ADD KEY `FK_CAR_PROD` (`COD_PROD`);
-
---
--- Indices de la tabla `cesta`
---
-ALTER TABLE `CESTA`
-  ADD PRIMARY KEY (`COD_USU`,`COD_PROD`),
-  ADD KEY `FK_CEST_PROD` (`COD_PROD`);
-
---
--- Indices de la tabla `lineadepedido`
---
-ALTER TABLE `LINEADEPEDIDO`
-  ADD PRIMARY KEY (`COD_LINEA`,`COD_PEDIDO`,`COD_PROD`),
-  ADD KEY `FK_PED_LP` (`COD_PEDIDO`),
-  ADD KEY `FK_PED_P` (`COD_PROD`);
-
---
--- Indices de la tabla `OPINION`
---
-ALTER TABLE `OPINION`
-  ADD PRIMARY KEY (`COD_OPINION`),
-  ADD KEY `FK_OP_USU` (`COD_USU`),
-  ADD KEY `FK_OP_PROD` (`COD_PROD`);
-
---
--- Indices de la tabla `PEDIDO`
---
-ALTER TABLE `PEDIDO`
-  ADD PRIMARY KEY (`COD_PEDIDO`),
-  ADD KEY `FK_PED_USU` (`COD_USU`);
-
---
--- Indices de la tabla `PRODUCTO`
---
-ALTER TABLE `PRODUCTO`
-  ADD PRIMARY KEY (`COD_PROD`);
-
---
--- Indices de la tabla `PROVEEDOR`
---
-ALTER TABLE `PROVEEDOR`
-  ADD PRIMARY KEY (`COD_PROV`);
-
---
--- Indices de la tabla `suministro`
---
-ALTER TABLE `SUMINISTRO`
-  ADD PRIMARY KEY (`COD_SUM`),
-  ADD KEY `FK_SUM_PROD` (`COD_PROD`),
-  ADD KEY `FK_SUM_PROV` (`COD_PROV`);
-
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `USUARIO`
-  ADD PRIMARY KEY (`COD_USU`),
-  ADD UNIQUE KEY `USERNAME` (`USERNAME`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `caracteristicas`
---
-ALTER TABLE `CARACTERISTICAS`
-  MODIFY `COD_CARAC` int(6) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=34;
---
--- AUTO_INCREMENT de la tabla `lineadepedido`
---
-ALTER TABLE `LINEADEPEDIDO`
-  MODIFY `COD_LINEA` int(8) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=35;
---
--- AUTO_INCREMENT de la tabla `OPINION`
---
-ALTER TABLE `OPINION`
-  MODIFY `COD_OPINION` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=101;
---
--- AUTO_INCREMENT de la tabla `PEDIDO`
---
-ALTER TABLE `PEDIDO`
-  MODIFY `COD_PEDIDO` int(6) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT de la tabla `PRODUCTO`
---
-ALTER TABLE `PRODUCTO`
-  MODIFY `COD_PROD` int(8) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
---
--- AUTO_INCREMENT de la tabla `proveedor`
---
-ALTER TABLE `PROVEEDOR`
-  MODIFY `COD_PROV` int(4) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
---
--- AUTO_INCREMENT de la tabla `suministro`
---
-ALTER TABLE `SUMINISTRO`
-  MODIFY `COD_SUM` int(8) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=27;
---
--- AUTO_INCREMENT de la tabla `usuario`
---
-ALTER TABLE `USUARIO`
-  MODIFY `COD_USU` int(6) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=24;
 --
 -- Restricciones para tablas volcadas
 --
@@ -419,14 +348,14 @@ ALTER TABLE `CARACTERISTICAS`
   ADD CONSTRAINT `FK_CAR_PROD` FOREIGN KEY (`COD_PROD`) REFERENCES `PRODUCTO` (`COD_PROD`);
 
 --
--- Filtros para la tabla `cesta`
+-- Filtros para la tabla `CESTA`
 --
 ALTER TABLE `CESTA`
   ADD CONSTRAINT `FK_CEST_PROD` FOREIGN KEY (`COD_PROD`) REFERENCES `PRODUCTO` (`COD_PROD`),
   ADD CONSTRAINT `FK_CEST_USU` FOREIGN KEY (`COD_USU`) REFERENCES `USUARIO` (`COD_USU`);
 
 --
--- Filtros para la tabla `lineadepedido`
+-- Filtros para la tabla `LINEADEPEDIDO`
 --
 ALTER TABLE `LINEADEPEDIDO`
   ADD CONSTRAINT `FK_PED_LP` FOREIGN KEY (`COD_PEDIDO`) REFERENCES `PEDIDO` (`COD_PEDIDO`),
@@ -440,13 +369,13 @@ ALTER TABLE `OPINION`
   ADD CONSTRAINT `FK_OP_USU` FOREIGN KEY (`COD_USU`) REFERENCES `USUARIO` (`COD_USU`);
 
 --
--- Filtros para la tabla `pedido`
+-- Filtros para la tabla `PEDIDO`
 --
 ALTER TABLE `PEDIDO`
   ADD CONSTRAINT `FK_PED_USU` FOREIGN KEY (`COD_USU`) REFERENCES `USUARIO` (`COD_USU`);
 
 --
--- Filtros para la tabla `suministro`
+-- Filtros para la tabla `SUMINISTRO`
 --
 ALTER TABLE `SUMINISTRO`
   ADD CONSTRAINT `FK_SUM_PROD` FOREIGN KEY (`COD_PROD`) REFERENCES `PRODUCTO` (`COD_PROD`),
